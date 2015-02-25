22922f425033aa37a63a62b6bfee03794c881136
Nayeem Al Noman <malnoman@login0.localdomain>
2015-02-24 21:01:56 -0600

    I was able to compile this code.

--- nayeem.cpp
+++ nayeem.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
 
+using namespace std;
+
 int main()
 {
 
-cout << "Hello" <<endl;
+cout << "I was able to compile this code using the HPC at the University of Memphis. When I compiled it there, it did not produce\n";
+cout << "any warning message. The HPC uses a GNU C++ compiler that can be considered a good up-to-date standard. I also\n";
+cout << "version-controlled this code using git, and used a remote repository hosted by github. If I can do this, so can you!!!\n";
 
 }
